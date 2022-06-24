392038d5c0c7ae48464f518c94fd82d0625b10c2
murilo-goncalves <murilogoncalves127@gmail.com>
2022-06-24 02:34:30 -0300

    remove all .mp4 files when completed downloading one audio

--- psybot/psybot.py
+++ psybot/psybot.py
@@ -4,8 +4,39 @@ import os
 from tkinter import Tk, Label, Entry, Button, Listbox
 from collections import deque
 import threading
+from pathlib import Path
+
+class EntryWithPlaceholder(Entry):
+    def __init__(self, master, width, placeholder="PLACEHOLDER", color='grey'):
+        super().__init__(master, width=width)
+
+        self.placeholder = placeholder
+        self.placeholder_color = color
+        self.default_fg_color = self['fg']
+
+        self.bind("<FocusIn>", self.foc_in)
+        self.bind("<FocusOut>", self.foc_out)
+
+        self.put_placeholder()
+
+    def put_placeholder(self):
+        self.insert(0, self.placeholder)
+        self['fg'] = self.placeholder_color
+
+    def foc_in(self, *args):
+        if self['fg'] == self.placeholder_color:
+            self.delete('0', 'end')
+            self['fg'] = self.default_fg_color
+
+    def foc_out(self, *args):
+        if not self.get():
+            self.put_placeholder()
 
 class Psybot:
+    WIDTH = 40
+    PADDING = 10
+    BTN_PADDING = 5
+
     def __init__(self, master):
         master.title("Psybot")
         self.master = master
@@ -14,20 +45,23 @@ class Psybot:
 
         self.set_ui()
 
-        self.master.bind('<Return>', self.btnAction)
+        self.master.bind("<Return>", self.btnAction)
 
     def set_ui(self):
-        self.intro_lbl = Label(self.master, text="ATENÇÃO! USE APENAS PARA PSYTRANCE")
-        self.intro_lbl.pack()
+        self.intro_lbl = Label(self.master, 
+                                text="ATENÇÃO! USE APENAS PARA PSYTRANCE",
+                                wraplength=200, 
+                                justify="center")
+        self.intro_lbl.pack(padx=self.PADDING, pady=self.PADDING)
 
-        self.entry = Entry(self.master)
-        self.entry.pack()
+        self.entry = EntryWithPlaceholder(self.master, width=self.WIDTH, placeholder="Insira um baile")
+        self.entry.pack(fill="x", padx=self.PADDING)
 
         self.button = Button(self.master, text="Loucura pura", command=self.btnAction)
-        self.button.pack()
+        self.button.pack(pady=self.BTN_PADDING)
 
-        self.list_box = Listbox(self.master)
-        self.list_box.pack()
+        self.list_box = Listbox(self.master, width=self.WIDTH, justify="center")
+        self.list_box.pack(fill="both", expand=True, padx=self.PADDING, pady=self.PADDING)
 
     def btnAction(self, event=None):
         search_input = self.entry.get()
@@ -61,13 +95,18 @@ class Psybot:
         try: 
             clip = VideoFileClip(video_path)
             clip.audio.write_audiofile(title + ".mp3")
-            os.remove(video_path)
+            clip.close()
+            self.clean_folder()
             self.list_box.delete(0)
         except Exception as e:
-            os.remove(video_path)
+            clip.close()
+            self.clean_folder()
             self.list_box.delete(0)
             print(e)
 
+    def clean_folder(self):
+        for filename in Path(".").glob("*.mp4"):
+            os.remove(filename)
 def main():
     root= Tk()
     psybot = Psybot(root)
